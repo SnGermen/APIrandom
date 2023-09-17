322bc5d862c86458c160fb14f51a78f7714b9ee8
SnGermen <144361057+SnGermen@users.noreply.github.com>
2023-09-17 13:20:02 +0200

    Api
    xyi

--- random cat/js/pp.js
+++ random cat/js/pp.js
@@ -1,14 +1,26 @@
 const button = document.querySelector(".btn");
 const image = document.querySelector(".img");
-const url = "https://random.cat/view/985";
+const card = document.querySelector(".card");
+const loadder = document.querySelector(".loadder");
+const url = 'https://api.thecatapi.com/v1/images/search?size=full'
 
 async function fetchHandler() {
   try {
+    button.disabled = true
+    loadder.className = 'loadder'
+    card.className = 'card card_hidden'
     const response = await fetch(url);
     const data = await response.json();
-    image.src = data.file;
+    image.src = data[0].url
+    image.alt = data[0].id
   } catch (error) {
     console.log(error);
+  } finally {
+    setTimeout(() => {
+      button.disabled = false
+      card.className = 'card'
+      loadder.className = 'loadder_hidden'
+    }, 2800);
   }
 }
 
